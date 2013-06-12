d1572b1be1b036ba1daddd451fe02d46b8e6677d
Pete Smith <roysvork@gmail.com>
2013-06-12 13:48:36 +0100

    Updated for Linq to Querystring v0.6.1 and latest jquery.odataqueryui version

--- LinqToQuerystringFilteringSample/App_Start/BundleConfig.cs
+++ LinqToQuerystringFilteringSample/App_Start/BundleConfig.cs
@@ -8,8 +8,7 @@
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.ODataFilterUI*"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/ko").Include(
                         "~/Scripts/knockout-{version}.js"));
@@ -20,6 +19,9 @@
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/odata").Include(
+                       "~/Scripts/jquery.ODataFilterUI*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
